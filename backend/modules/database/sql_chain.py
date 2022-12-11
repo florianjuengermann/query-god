6759901ddb78b79a1c09322dc5e20d5eb1513d77
Florian Juengermann <florianjuengermann@gmail.com>
2022-12-11 01:55:15 -0500

    fixed empty result bug

--- backend/modules/database/sql_chain.py
+++ backend/modules/database/sql_chain.py
@@ -97,13 +97,15 @@ class SQLDatabaseChain(Chain, BaseModel):
         self.custom_memory = {
             "query_result": result,
             "sql_cmd": sql_cmd,
-            "query_result_summary": {
+        }
+        if len(result) > 3:
+            self.custom_memory["query_result_summary"] = {
                 "type": "json file",
                 "name": "query_result.json",
                 "outline": f"{len(result)} rows",
                 "example": f"[{result[0]}, ...]",
                 "source": sql_cmd,
                 "text": lang_output,
-            },
-        }
+            }
+
         return {self.output_key: final_result}
